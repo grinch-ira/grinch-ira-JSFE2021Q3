077f0034d3cb9572e577e493e77f06ca4ca72644
Hryniuk Iryna <grinch.ira@gmail.com>
2021-10-18 17:16:18 +0300

    feat: add getTimeOfDay function

--- momentum/script.js
+++ momentum/script.js
@@ -13,6 +13,7 @@ function showTime(){
     document.querySelector('.time').innerHTML = currentTime;
     setTimeout(showTime,1000);
     showDate();
+    getTimeOfDay();
 }
 showTime();
 
@@ -26,4 +27,37 @@ function showDate(){
 }
 showDate();
 
+function getTimeOfDay(){
+    let timeOfDay;
+    const date = new Date();
+    const hours = date.getHours();
+    if(hours>=0 && hours<6){
+        timeOfDay = `Night`
+    }
+    else if(hours>=6 && hours<12){
+        timeOfDay = `Morning`
+    }
+    else if(hours>=12 && hours<18){
+        timeOfDay = `Day`
+    }
+    else if(hours>=18 && hours<24){
+        timeOfDay = `Evening`
+    }
+return document.querySelector('.greeting').innerHTML = `Good ${timeOfDay},` ;
+}
+getTimeOfDay();
 
+
+
+
+// function setLocalStorage() {
+//     localStorage.setItem('name', name.value);
+//   }
+//   window.addEventListener('beforeunload', setLocalStorage)
+
+//   function getLocalStorage() {
+//     if(localStorage.getItem('name')) {
+//       name.value = localStorage.getItem('name');
+//     }
+//   }
+//   window.addEventListener('load', getLocalStorage)
